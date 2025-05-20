1ffd7a28d66ff8538c40d9b322f40805d0c4c107
Juliana Busato <busatoju@gmail.com>
2025-05-20 19:43:59 -0300

    Criação do layout

--- AppListaVip/app/src/main/java/dev/busato/applistavip/MainActivity.java
+++ AppListaVip/app/src/main/java/dev/busato/applistavip/MainActivity.java
@@ -1,6 +1,9 @@
 package dev.busato.applistavip;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText firstNameInput;
+    private EditText lastNameInput;
+    private EditText courseNameInput;
+    private EditText phoneInput;
+
+    private Button  clearButton;
+    private Button saveButton;
+    private Button finishButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,33 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        iniciarComponentesDeLayout();
+    }
+
+
+    private void iniciarComponentesDeLayout(){
+        firstNameInput = findViewById(R.id.firstNameInput);
+        lastNameInput = findViewById(R.id.lastNameInput);
+        courseNameInput = findViewById(R.id.courseNameInput);
+        phoneInput = findViewById(R.id.phoneInput);
+
+        clearButton = findViewById(R.id.clearButton);
+        saveButton = findViewById(R.id.saveButton);
+        finishButton = findViewById(R.id.finishButton);
+    }
+
+
+    private void clearButton(){
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstNameInput.getText().clear();
+                lastNameInput.getText().clear();
+                courseNameInput.getText().clear();
+                phoneInput.getText().clear();
+            }
         });
     }
 }
